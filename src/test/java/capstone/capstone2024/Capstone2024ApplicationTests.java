494463c25b276cfb942accf0986961e693bc7b3d
pstar987 <pstar987@naver.com>
2024-03-21 12:35:53 +0900

    first commit

---
+++ src/test/java/capstone/capstone2024/Capstone2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.capstone2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Capstone2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
