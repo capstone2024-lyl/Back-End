2cad7fe1294362fcc82a013067c25748971fa640
pstar987 <pstar987@naver.com>
2024-05-07 14:08:25 +0900

    feat : 스웨거 설정 정리

--- src/main/java/capstone/capstone2024/global/config/SwaggerConfig.java
+++ src/main/java/capstone/capstone2024/global/config/SwaggerConfig.java
@@ -1,17 +1,15 @@
 package capstone.capstone2024.global.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableWebMvc
+
 public class SwaggerConfig {
 
     @Bean
