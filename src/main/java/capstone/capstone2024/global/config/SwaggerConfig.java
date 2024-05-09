41a35fd6c7f588fc86fc676052c61d41f67e2563
pstar987 <pstar987@naver.com>
2024-05-09 22:32:38 +0900

    feat : 스웨거 상에서 토큰 authorization 사용을 위한 설정 추가

--- src/main/java/capstone/capstone2024/global/config/SwaggerConfig.java
+++ src/main/java/capstone/capstone2024/global/config/SwaggerConfig.java
@@ -1,11 +1,17 @@
 package capstone.capstone2024.global.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebMvc
@@ -14,11 +20,18 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        SecurityScheme securityScheme = new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+
         return new OpenAPI()
                 .info(new Info()
                         .title("2024-1 capstone API")
                         .description("LYL 팀 2024-1 capstone API 문서")
-                        .version("1.0.0"));
+                        .version("1.0.0"))
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme)).security(Arrays.asList(securityRequirement));
 //                .components(new Components()
 //                        .addSecuritySchemes("bearer-key",
 //                                new io.swagger.v3.oas.models.security.SecurityScheme()
