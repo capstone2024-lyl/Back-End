391a127ee6ca957be865eb90a657b71cde9a27e8
pstar987 <pstar987@naver.com>
2024-05-12 20:37:26 +0900

    feat : MBTI enum으로 선언해서 사용

---
+++ src/main/java/capstone/capstone2024/domain/chat/domain/MBTI.java
@@ -0,0 +1,8 @@
+package capstone.capstone2024.domain.chat.domain;
+
+public enum MBTI {
+    ENTJ, ENFP, ENFJ, ISTP,
+    ISTJ, ISFP, ISFJ, INTP,
+    INTJ, INFP, INFJ, ESTP,
+    ESTJ, ESFP, ESFJ, ENTP;
+}
