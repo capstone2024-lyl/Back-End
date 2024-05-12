2e2e9b74e3d967f5e7198fc43e19a1c3bd2d6617
pstar987 <pstar987@naver.com>
2024-05-12 20:39:26 +0900

    feat : chat의 엔티티 및 reopsitory

--- src/main/java/capstone/capstone2024/domain/chat/domain/Chat.java
+++ src/main/java/capstone/capstone2024/domain/chat/domain/Chat.java
@@ -1,37 +1,32 @@
-//package capstone.capstone2024.domain.chat.domain;
-//import capstone.capstone2024.domain.base.BaseEntity;
-//import capstone.capstone2024.domain.user.domain.User;
-//import jakarta.persistence.*;
-//import lombok.*;
-//
-//@Entity
-//@Builder
-//@Getter
-//@NoArgsConstructor(access = AccessLevel.PROTECTED)
-//@AllArgsConstructor
-//public class Chat extends BaseEntity {
-//
-//    @Column(name = "neur")
-//    private Integer neur;
-//
-//
-//    @Column(name = "extr")
-//    private Integer extr;
-//
-//
-//    @Column(name = "open")
-//    private Integer open;
-//
-//
-//    @Column(name = "agree")
-//    private Integer agree;
-//
-//
-//    @Column(name = "cons")
-//    private Integer cons;
-//
-//
-//    @JoinColumn(name = "user_id")
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    private User user;
-//}
+package capstone.capstone2024.domain.chat.domain;
+import capstone.capstone2024.domain.base.BaseEntity;
+import capstone.capstone2024.domain.user.domain.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Chat extends BaseEntity {
+
+    @Column(name = "energy")
+    private Integer energy;
+
+    @Column(name = "recognition")
+    private Integer recognition;
+
+    @Column(name = "decision")
+    private Integer decision;
+
+    @Column(name = "lifeStyle")
+    private Integer lifeStyle;
+
+    @Column(name = "mbti")
+    private Enum mbti;
+
+    @Column(name = "isChecked")
+    private Boolean isCheckd;
+
+}
--- src/main/java/capstone/capstone2024/domain/chat/domain/ChatRepository.java
+++ src/main/java/capstone/capstone2024/domain/chat/domain/ChatRepository.java
@@ -1,4 +1,10 @@
 package capstone.capstone2024.domain.chat.domain;
 
-public class ChatRepository {
+import capstone.capstone2024.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ChatRepository extends JpaRepository<Chat, Long> {
+    Optional<Chat> findById(Long chatId);
 }
