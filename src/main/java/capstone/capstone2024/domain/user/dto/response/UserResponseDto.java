06766629f44beda6faf6fa62ffbe34cd803c26d1
pstar987 <pstar987@naver.com>
2024-05-14 23:08:12 +0900

    feat : findUser실행 시 앱 사용기록 top10 반환을 위한 타입 AppResponseDto로 변경

--- src/main/java/capstone/capstone2024/domain/user/dto/response/UserResponseDto.java
+++ src/main/java/capstone/capstone2024/domain/user/dto/response/UserResponseDto.java
@@ -1,10 +1,12 @@
 package capstone.capstone2024.domain.user.dto.response;
 
 import capstone.capstone2024.domain.app.domain.App;
+import capstone.capstone2024.domain.app.dto.response.AppResponseDto;
 import capstone.capstone2024.domain.category.domain.Category;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
+import org.springframework.data.domain.Page;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -21,7 +23,7 @@ public class UserResponseDto {
 
     private String mbti;
 
-    private List<App> app;
+    private List<AppResponseDto> apps;
 
     private List<Category> category;
 
