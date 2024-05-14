3ee499e0aeb61ca0084fa45a5f83b6d6eb400286
pstar987 <pstar987@naver.com>
2024-05-14 16:57:42 +0900

    feat : 앱 사용기록 관리 엔티티 정보 변경

--- src/main/java/capstone/capstone2024/domain/app/domain/App.java
+++ src/main/java/capstone/capstone2024/domain/app/domain/App.java
@@ -15,17 +15,11 @@ import java.util.List;
 @AllArgsConstructor
 public class App extends BaseEntity {
 
-    @Column(name = "appName")
-    private String appName;
-
     @Column(name = "appPackageName")
     private String appPackageName;
 
     @Column(name = "usageTime", nullable = false)
     private Integer usageTime;
-
-    @Column(name = "appUrl")
-    private String appUrl;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "userId")
--- src/main/java/capstone/capstone2024/domain/app/dto/request/AppUsageCreateRequestDto.java
+++ src/main/java/capstone/capstone2024/domain/app/dto/request/AppUsageCreateRequestDto.java
@@ -1,24 +1,28 @@
 package capstone.capstone2024.domain.app.dto.request;
 
 import capstone.capstone2024.domain.app.domain.App;
+import capstone.capstone2024.domain.user.domain.User;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
+@NoArgsConstructor
 public class AppUsageCreateRequestDto {
-    private String appName;
-    private String appPackageName;
-    private Integer usageTime;
-    private String appUrl;
 
-    public App toEntity() {
+    @NotBlank(message = "앱 패키지명이 비어있습니다.")
+    private String appPackageName;
+
+    @NotNull(message = "앱 사용시간이 비어있습니다.")
+    private Integer usageTime;
+
+    public App toEntity(User user) {
         return App.builder()
-                .appName(this.appName)
                 .usageTime(this.usageTime)
                 .appPackageName(this.appPackageName)
-                .appUrl(this.appUrl)
+                .user(user)
                 .build();
     }
 }
