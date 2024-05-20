bd6b4497b4a870be2e13b21888eb39ebcfecd101
pstar987 <pstar987@naver.com>
2024-05-20 14:09:29 +0900

    feat : 앱 사용시간 평균값 분단위로 설정

--- src/main/java/capstone/capstone2024/domain/app/domain/AppCategory.java
+++ src/main/java/capstone/capstone2024/domain/app/domain/AppCategory.java
@@ -4,20 +4,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum AppCategory {
-    GAME("게임"),
-    MUSIC("음악"),
-    NEWS("뉴스"),
-    VIDEO("동영상"),
-    SNS("SNS"),
-    WEBTOON("웹툰"),
-    CHAT("채팅"),
-    MAP("지도"),
-    LIFESTYLE("생활정보"),
-    PHONE_DECORATION("폰꾸미기"),
-    OTHERS("기타"),
-    E_BOOK("e-book");
+    GAME("게임", 180),
+    MUSIC("음악", 240),
+    NEWS("뉴스", 20),
+    VIDEO("동영상", 720),
+    SNS("SNS", 960),
+    WEBTOON("웹툰", 120),
+    CHAT("채팅", 120),
+    MAP("지도", 180),
+    LIFESTYLE("생활정보", 10),
+    PHONE_DECORATION("폰꾸미기", 10),
+    OTHERS("기타", 10),
+    E_BOOK("e-book", 120);
 
     private final String description;
+    private final Integer averageUsage;
     private static final Map<String, AppCategory> packageToCategoryMap = new HashMap<>();
 
     static {
@@ -28,15 +29,20 @@ public enum AppCategory {
         // 추가적인 패키지명과 카테고리 매핑도 여기에 추가할 수 있습니다.
     }
 
-    AppCategory(String description) {
+    AppCategory(String description, Integer averageUsage) {
         this.description = description;
+        this.averageUsage = averageUsage;
     }
 
     public String getDescription() {
         return description;
     }
 
+    public double getAverageUsage() {return averageUsage; }
+
     public static AppCategory fromPackageName(String packageName) {
         return packageToCategoryMap.getOrDefault(packageName, OTHERS);
     }
+
+
 }
