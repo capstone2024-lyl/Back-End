587c2ce41a817c67cad8ee2835bee33b14e2ad64
pstar987 <pstar987@naver.com>
2024-05-20 18:25:13 +0900

    feat : 구글 액세스 토큰을 받아서 유튜브 api 사용 setCacheControl를 사용하여서 캐시를 무시하도록 설정(이 설정으로 구현 성공함)

--- src/main/java/capstone/capstone2024/domain/youtube/application/YoutubeService.java
+++ src/main/java/capstone/capstone2024/domain/youtube/application/YoutubeService.java
@@ -1,6 +1,8 @@
 package capstone.capstone2024.domain.youtube.application;
 
+import capstone.capstone2024.domain.youtube.dto.YoutubeSubscribeResponseDto;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpHeaders;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.Subscription;
@@ -10,22 +12,37 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class YoutubeService {
-    public List<Subscription> getSubscriptions(String accessToken) {
+    public List<YoutubeSubscribeResponseDto> getSubscriptions(String googleAccessToken) {
         try {
             YouTube youtube = new YouTube.Builder(GoogleNetHttpTransport.newTrustedTransport(), JacksonFactory.getDefaultInstance(), request -> {
-                request.getHeaders().setAuthorization("Bearer " + accessToken);
+                request.getHeaders().setAuthorization("Bearer " + googleAccessToken);
             }).setApplicationName("youtube-subscriptions").build();
 
             YouTube.Subscriptions.List request = youtube.subscriptions().list("snippet,contentDetails");
             request.setMine(true);
             request.setMaxResults(10L);
+            request.setRequestHeaders(new HttpHeaders().setCacheControl("no-cache"));
+            request.setOauthToken(googleAccessToken);
+
+
 
             SubscriptionListResponse response = request.execute();
-            return response.getItems();
+            List<Subscription> subscriptions = response.getItems();
+
+            return subscriptions.stream()
+                    .map(subscription -> YoutubeSubscribeResponseDto.builder()
+                            .id(subscription.getId())
+                            .title(subscription.getSnippet().getTitle())
+                            .description(subscription.getSnippet().getDescription())
+                            .channelId(subscription.getSnippet().getResourceId().getChannelId())
+                            .build())
+                    .collect(Collectors.toList());
         } catch (GeneralSecurityException | IOException e) {
+            System.out.println("e.getMessage() = " + e.getMessage());
             throw new RuntimeException("Failed to retrieve subscriptions", e);
         }
     }
