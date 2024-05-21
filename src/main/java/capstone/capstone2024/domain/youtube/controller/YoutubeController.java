82d918803d77100f4241c55f76c35128ada1ce3b
pstar987 <pstar987@naver.com>
2024-05-21 12:21:45 +0900

    feat : 현재 사용자의 유튜브 구독 목록을 저장할 수 있게 수정

--- src/main/java/capstone/capstone2024/domain/youtube/controller/YoutubeController.java
+++ src/main/java/capstone/capstone2024/domain/youtube/controller/YoutubeController.java
@@ -1,10 +1,11 @@
 package capstone.capstone2024.domain.youtube.controller;
 
 import capstone.capstone2024.domain.youtube.application.YoutubeService;
-import capstone.capstone2024.domain.oauth2.application.OAuth2Service;
-import capstone.capstone2024.domain.youtube.dto.YoutubeSubscribeResponseDto;
+import capstone.capstone2024.domain.youtube.dto.response.YoutubeSubscribeResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +23,9 @@ public class YoutubeController {
     public ResponseEntity<List<YoutubeSubscribeResponseDto>> getSubscriptions(
             @RequestHeader("X-Google-Token") String googleToken
     ) {
-        return ResponseEntity.ok(youtubeService.getSubscriptions(googleToken));
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String loginId = authentication.getName();
+        return ResponseEntity.ok(youtubeService.getSubscriptions(googleToken, loginId));
     }
 
 
