1ebceab9abe33ad62a3414f254f12077923ce9ae
pstar987 <pstar987@naver.com>
2024-05-21 12:22:35 +0900

    feat : 구독목록 저장을 위한 수정

--- src/main/java/capstone/capstone2024/domain/youtube/application/YoutubeService.java
+++ src/main/java/capstone/capstone2024/domain/youtube/application/YoutubeService.java
@@ -1,23 +1,41 @@
 package capstone.capstone2024.domain.youtube.application;
 
-import capstone.capstone2024.domain.youtube.dto.YoutubeSubscribeResponseDto;
+import capstone.capstone2024.domain.openai.application.OpenAIService;
+import capstone.capstone2024.domain.user.domain.User;
+import capstone.capstone2024.domain.user.domain.UserRepository;
+import capstone.capstone2024.domain.youtube.domain.Youtube;
+import capstone.capstone2024.domain.youtube.domain.YoutubeRepository;
+import capstone.capstone2024.domain.youtube.dto.response.YoutubeSubscribeResponseDto;
+import capstone.capstone2024.global.error.exceptions.BadRequestException;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpHeaders;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.Subscription;
 import com.google.api.services.youtube.model.SubscriptionListResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static capstone.capstone2024.global.error.ErrorCode.ROW_DOES_NOT_EXIST;
+
 @Service
+@RequiredArgsConstructor
 public class YoutubeService {
-    public List<YoutubeSubscribeResponseDto> getSubscriptions(String googleAccessToken) {
+    private final UserRepository userRepository;
+    private final YoutubeRepository youtubeRepository;
+
+    @Transactional
+    public List<YoutubeSubscribeResponseDto> getSubscriptions(String googleAccessToken, String loginId) {
         try {
+            User user = userRepository.findByLoginId(loginId)
+                    .orElseThrow(() -> new BadRequestException(ROW_DOES_NOT_EXIST, "존재하지 않는 아이디입니다."));
+
             YouTube youtube = new YouTube.Builder(GoogleNetHttpTransport.newTrustedTransport(), JacksonFactory.getDefaultInstance(), request -> {
                 request.getHeaders().setAuthorization("Bearer " + googleAccessToken);
             }).setApplicationName("youtube-subscriptions").build();
@@ -33,12 +51,26 @@ public class YoutubeService {
             SubscriptionListResponse response = request.execute();
             List<Subscription> subscriptions = response.getItems();
 
+//            // 구독 채널의 타이틀을 줄바꿈으로 구분하여 문자열로 생성
+//            String channelTitles = subscriptions.stream()
+//                    .map(subscription -> subscription.getSnippet().getTitle())
+//                    .collect(Collectors.joining("\n"));
+////            createChannelCategory(channelTitles);
+
+
+            List<Youtube> youtubeEntities = subscriptions.stream()
+                    .map(subscription -> Youtube.builder()
+                            .channelName(subscription.getSnippet().getTitle())
+                            .user(user)
+                            .build())
+                    .collect(Collectors.toList());
+
+            youtubeRepository.saveAll(youtubeEntities);
+
+
             return subscriptions.stream()
                     .map(subscription -> YoutubeSubscribeResponseDto.builder()
-                            .id(subscription.getId())
                             .title(subscription.getSnippet().getTitle())
-                            .description(subscription.getSnippet().getDescription())
-                            .channelId(subscription.getSnippet().getResourceId().getChannelId())
                             .build())
                     .collect(Collectors.toList());
         } catch (GeneralSecurityException | IOException e) {
@@ -46,4 +78,10 @@ public class YoutubeService {
             throw new RuntimeException("Failed to retrieve subscriptions", e);
         }
     }
+
+//    public void createChannelCategory(String channelTitles){
+//        String subscibeResponse = openAIService.youtubeSearch(channelTitles);
+//
+//    }
+
 }
