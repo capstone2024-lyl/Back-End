82dede2de8a9e46784b247795d163973f0852087
pstar987 <pstar987@naver.com>
2024-05-21 12:22:02 +0900

    feat : YoutubeRepository 생성

---
+++ src/main/java/capstone/capstone2024/domain/youtube/domain/YoutubeRepository.java
@@ -0,0 +1,10 @@
+package capstone.capstone2024.domain.youtube.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface YoutubeRepository extends JpaRepository<Youtube, Long> {
+
+}
