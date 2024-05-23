58749d02c622f33ff3534b07476f7984e9c80ec6
pstar987 <pstar987@naver.com>
2024-05-23 20:17:32 +0900

    feat : controller 수정 전체 기능상 predict-mbti 와 findMBTI만 사용 fileUploadAndTranslate와 fileUploadandAnalyze는 프롬프트 테스트용으로 생성

--- src/main/java/capstone/capstone2024/domain/chat/controller/ChatController.java
+++ src/main/java/capstone/capstone2024/domain/chat/controller/ChatController.java
@@ -5,6 +5,7 @@ import capstone.capstone2024.domain.chat.application.ChatService;
 
 import capstone.capstone2024.domain.chat.dto.response.ChatPredictResponseDto;
 import capstone.capstone2024.domain.chat.dto.response.ChatResponseDto;
+import capstone.capstone2024.global.payload.ApiResponseTemplate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +14,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,15 +21,37 @@ import java.io.IOException;
 public class ChatController {
     private final ChatService chatService;
 
-    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<String> fileUpload(
-            @RequestParam("file") MultipartFile file,
-            @RequestParam("path") String path
+    @PostMapping(value = "/predict-mbti", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ChatPredictResponseDto> fileUploadAndPredictMBTI(
+            @RequestParam("file") MultipartFile file
     ) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String loginId = authentication.getName();
-        return ResponseEntity.ok(chatService.filterChat(file, loginId, path));
+        return ResponseEntity.ok(chatService.uploadChat(file, loginId));
     }
+
+    @PostMapping(value = "/translate", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> fileUploadAndTranslate(
+            @RequestParam("file") MultipartFile file
+    ) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String loginId = authentication.getName();
+        String translatedMessages = chatService.translateChat(file, loginId);
+        return ResponseEntity.ok(translatedMessages);
+    }
+
+    @PostMapping(value = "/analyze", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ApiResponseTemplate<String> fileUploadAndAnalyze(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("mbti") String mbti
+    ) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String loginId = authentication.getName();
+        String translatedMessages = chatService.analyzeChat(file, loginId, mbti);
+        return ApiResponseTemplate.ok(translatedMessages);
+    }
+
+
 
     @GetMapping("/findMBTI")
     public ResponseEntity<ChatResponseDto> findMBTI(){
@@ -38,11 +60,6 @@ public class ChatController {
         return ResponseEntity.ok(chatService.findMBTI(loginId));
     }
 
-    @PostMapping(value = "/predict_mbti", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<ChatPredictResponseDto> predictMBTI(@RequestParam("file") MultipartFile file) {
-
-        return ResponseEntity.ok(chatService.predictMBTI(file));
-    }
 
 
 
