4e9a63da319a73af6970539c014b96f7b8421b4b
pstar987 <pstar987@naver.com>
2024-05-23 19:57:49 +0900

    feat : user엔티티와 양방향 매핑 및 검사 결과를 업데이트 하기위한 update 추가

--- src/main/java/capstone/capstone2024/domain/chat/domain/Chat.java
+++ src/main/java/capstone/capstone2024/domain/chat/domain/Chat.java
@@ -23,10 +23,36 @@ public class Chat extends BaseEntity {
     @Column(name = "lifeStyle")
     private Integer lifeStyle;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "mbti")
-    private Enum mbti;
+    private MBTI mbti;
 
     @Column(name = "isChecked")
-    private Boolean isCheckd;
+    private Boolean isChecked;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @Column(name = "chatCount")
+    private Integer chatCount;
+
+    public void update(
+            Integer energy,
+            Integer recognition,
+            Integer decision,
+            Integer lifeStyle,
+            MBTI mbti,
+            Boolean isChecked,
+            Integer chatCount
+    ){
+        this.energy = energy;
+        this.recognition = recognition;
+        this.decision = decision;
+        this.lifeStyle = lifeStyle;
+        this.mbti = mbti;
+        this.isChecked = isChecked;
+        this.chatCount = chatCount;
+    }
 
 }
