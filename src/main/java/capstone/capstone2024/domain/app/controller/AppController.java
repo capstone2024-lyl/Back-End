c6f81bf93819dbac3949f568bc66139ac15a7f5c
pstar987 <pstar987@naver.com>
2024-05-24 17:23:07 +0900

    feat : AppsResponseDto 반환으로 변경

--- src/main/java/capstone/capstone2024/domain/app/controller/AppController.java
+++ src/main/java/capstone/capstone2024/domain/app/controller/AppController.java
@@ -3,6 +3,7 @@ package capstone.capstone2024.domain.app.controller;
 import capstone.capstone2024.domain.app.application.AppService;
 import capstone.capstone2024.domain.app.dto.request.AppUsageCreateRequestDto;
 import capstone.capstone2024.domain.app.dto.response.AppResponseDto;
+import capstone.capstone2024.domain.app.dto.response.AppsResponseDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +19,7 @@ import java.util.List;
 public class AppController {
     private final AppService appService;
     @GetMapping("/findTop10")
-    public ResponseEntity<List<AppResponseDto>> findTop10App(){
+    public ResponseEntity<AppsResponseDto> findTop10App(){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String loginId = authentication.getName();
         return ResponseEntity.ok(appService.findTop10App(loginId));
