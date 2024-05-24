3da8a8ef765a43a3dd91fbe97f21daf685690dbd
pstar987 <pstar987@naver.com>
2024-05-24 17:21:48 +0900

    feat : userId로 사용자 검색

--- src/main/java/capstone/capstone2024/domain/chat/domain/ChatRepository.java
+++ src/main/java/capstone/capstone2024/domain/chat/domain/ChatRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
-    Optional<Chat> findById(Long chatId);
+    Optional<Chat> findByUserId(Long userId);
 }
