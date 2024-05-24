530fb4baa634be4e3d2baa4ac94e37d6c1bc64bd
pstar987 <pstar987@naver.com>
2024-05-24 17:12:46 +0900

    feat : youtube 구독목록을 카테고리별로 분류해서 카운팅 하기위한 엔티티 생성

---
+++ src/main/java/capstone/capstone2024/domain/youtube/domain/YoutubeCategories.java
@@ -0,0 +1,23 @@
+package capstone.capstone2024.domain.youtube.domain;
+
+import capstone.capstone2024.domain.base.BaseEntity;
+import capstone.capstone2024.domain.user.domain.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class YoutubeCategories extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    private YoutubeCategory category;
+
+    private Long categoryCount;
+}
---
+++ src/main/java/capstone/capstone2024/domain/youtube/domain/YoutubeCategoriesRepository.java
@@ -0,0 +1,10 @@
+package capstone.capstone2024.domain.youtube.domain;
+
+import capstone.capstone2024.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface YoutubeCategoriesRepository extends JpaRepository<YoutubeCategories, Long> {
+    List<YoutubeCategories> findByUserId(Long userId);
+}
