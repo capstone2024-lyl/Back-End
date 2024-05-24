a0515c2d3ac3391641e00f19417288c6a196f72c
pstar987 <pstar987@naver.com>
2024-05-24 17:17:34 +0900

    feat : 유튜브 서비스에 youtubeService와 chatService를 주입하여 findUser 시 유저정보 반환에 사용

--- src/main/java/capstone/capstone2024/domain/user/application/UserService.java
+++ src/main/java/capstone/capstone2024/domain/user/application/UserService.java
@@ -1,19 +1,20 @@
 package capstone.capstone2024.domain.user.application;
 
-//import capstone.capstone2024.domain.app.domain.AppRepository;
-//import capstone.capstone2024.domain.category.domain.CategoryRepository;
-import capstone.capstone2024.domain.app.application.AppService;
 import capstone.capstone2024.domain.app.domain.App;
 import capstone.capstone2024.domain.app.domain.AppRepository;
 import capstone.capstone2024.domain.app.dto.response.AppResponseDto;
+import capstone.capstone2024.domain.app.dto.response.AppsResponseDto;
+import capstone.capstone2024.domain.chat.application.ChatService;
+import capstone.capstone2024.domain.chat.dto.response.ChatResponseDto;
 import capstone.capstone2024.domain.user.domain.User;
-import capstone.capstone2024.domain.user.domain.UserNickname;
+import capstone.capstone2024.domain.nickname.domain.Nickname;
 import capstone.capstone2024.domain.user.domain.UserRepository;
 import capstone.capstone2024.domain.user.dto.request.UserCreateRequestDto;
 import capstone.capstone2024.domain.user.dto.request.UserLogInRequestDto;
 import capstone.capstone2024.domain.user.dto.response.UserLoginResponseDto;
-import capstone.capstone2024.domain.user.dto.response.UserNicknameResponseDto;
 import capstone.capstone2024.domain.user.dto.response.UserResponseDto;
+import capstone.capstone2024.domain.youtube.application.YoutubeService;
+import capstone.capstone2024.domain.youtube.dto.response.YoutubeTop3CategoriesResponseDto;
 import capstone.capstone2024.global.auth.JwtTokenUtil;
 import capstone.capstone2024.global.error.exceptions.BadRequestException;
 import lombok.RequiredArgsConstructor;
@@ -34,6 +35,8 @@ public class UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder encoder;
     private final AppRepository appRepository;
+    private final YoutubeService youtubeService;
+    private final ChatService chatService;
 
     @Value("${spring.jwt.secret}")
     private String secretKey;
@@ -104,39 +107,41 @@ public class UserService {
                 .orElseThrow(() -> new BadRequestException(ROW_DOES_NOT_EXIST, "존재하지 않는 사용자입니다."));
 
 
-        List<App> apps = appRepository.findByUserIdOrderByUsageTimeDesc(user.getId());
+        List<App> appList = appRepository.findByUserIdOrderByUsageTimeDesc(user.getId());
+        List<AppResponseDto> appResponseDtos = appList.stream()
+                .map(app -> AppResponseDto.builder()
+                        .appPackageName(app.getAppPackageName())
+                        .usageTime(app.getUsageTime())
+                        .build())
+                .collect(Collectors.toList());
+
+        boolean isAppsChecked = !appResponseDtos.isEmpty();
+        AppsResponseDto appsResponseDto = AppsResponseDto.builder()
+                .apps(appResponseDtos)
+                .isChecked(isAppsChecked)
+                .build();
+
+        YoutubeTop3CategoriesResponseDto top3Categories = youtubeService.findTop3Categories(loginId);
+        ChatResponseDto mbtiInfo = chatService.findMBTI(loginId);
+
+        List<String> userNicknameValues = user.getNickname().stream()
+                .map(Nickname::getDescription)
+                .collect(Collectors.toList());
+        System.out.println("userNicknameValues = " + userNicknameValues);
 
         return UserResponseDto.builder()
                 .name(user.getName())
                 .birthday(user.getBirthday())
-                .apps(apps.stream()
-                        .map(app -> AppResponseDto.builder()
-                                .appPackageName(app.getAppPackageName())
-                                .usageTime(app.getUsageTime())
-                                .build())
-                        .collect(Collectors.toList()))
-                .nicknames(user.getNickname())
-//                .mbti(user.getMbti())
-
+                .apps(appsResponseDto)
+                .category(top3Categories)
+                .mbti(mbtiInfo)
+                .nicknames(userNicknameValues)
                 .build();
     }
 
 
-    @Transactional
-    public UserNicknameResponseDto addNickname(String loginId, UserNickname nickname) {
-        User user = userRepository.findByLoginId(loginId)
-                .orElseThrow(() -> new BadRequestException(ROW_DOES_NOT_EXIST, "존재하지 않는 사용자입니다."));
 
-        if (!user.getNickname().contains(nickname)) {
-            user.getNickname().add(nickname);
-            userRepository.save(user);
-        }
 
-        return UserNicknameResponseDto.builder()
-                .loginId(user.getLoginId())
-                .nicknames(user.getNickname())
-                .build();
-    }
 
 
 
