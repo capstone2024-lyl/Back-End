12b29e2f84520e13c9a49f80b299fb595ee641a8
pstar987 <pstar987@naver.com>
2024-05-26 12:37:04 +0900

    feat : 구독목록 저장 삭제

--- src/main/java/capstone/capstone2024/domain/youtube/domain/YoutubeSubscribeRepository.java
+++
@@ -1,7 +0,0 @@
-package capstone.capstone2024.domain.youtube.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface YoutubeSubscribeRepository extends JpaRepository<YoutubeSubscribe, Long> {
-
-}
