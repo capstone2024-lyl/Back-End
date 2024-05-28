e3c701190ea275f3aba4ba340bd22d3546ef643b
pstar987 <pstar987@naver.com>
2024-05-28 16:42:31 +0900

    feat : PhotoCategory enum 별도 생성

---
+++ src/main/java/capstone/capstone2024/domain/photo/domain/PhotoCategory.java
@@ -0,0 +1,23 @@
+package capstone.capstone2024.domain.photo.domain;
+
+public enum PhotoCategory {
+    NATURE("자연"),
+    PERSON("사람"),
+    ANIMAL("동물"),
+    VEHICLE("교통 수단"),
+    HOME_APPLIANCE("가전제품"),
+    FOOD("음식"),
+    FURNITURE("가구"),
+    DAILY("일상");
+
+    private final String value;
+
+    PhotoCategory(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
+
